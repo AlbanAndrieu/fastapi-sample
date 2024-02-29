8c3fd1156cc5a0a010e935ea185937879381f707
Amine Hajali <a.hajali@jusmundi.com>
2024-02-29 15:04:20 +0100

    Put JM Links

--- nabla/perf/locustfile_jm.py
+++ nabla/perf/locustfile_jm.py
@@ -14,90 +14,93 @@ class QuickstartUser(HttpUser):
 
     @task(2)
     def jm_front(self):
-        # self.client.get("/en", verify=False)
+        self.client.get("en/document/decision/fr-victor-pey-casado-and-president-allende-foundation-v-republic-of-chile-award-thursday-8th-may-2008#decision_849")
+        self.client.get("en/document/decision/fr-association-oxfam-france-association-notre-affaire-a-tous-fondation-pour-la-nature-et-lhomme-association-greenpeace-france-v-france-jugement-du-tribunal-administratif-de-paris-wednesday-3rd-february-2021")
+        self.client.get("en/document/treaty/en-libya-slovakia-bit-2009-libya-slovakia-bit-2009-friday-20th-february-2009")
+    #     # self.client.get("/en", verify=False)
 
-        self.client.get("/en/conflict-checker?type=p2lf&ref=13&to=1205")
+    #     self.client.get("/en/conflict-checker?type=p2lf&ref=13&to=1205")
 
-        # self.client.get(
-        #    "/en/search?page=1&lang=en&document-types%5B0%5D=treaty", verify=False
-        # )
+    #     # self.client.get(
+    #     #    "/en/search?page=1&lang=en&document-types%5B0%5D=treaty", verify=False
+    #     # )
 
-        self.client.get(
-            "/en/api/search?page=1&lang=en&document-types[0]=treaty", verify=False
-        )
+    #     self.client.get(
+    #         "/en/api/search?page=1&lang=en&document-types[0]=treaty", verify=False
+    #     )
 
-        # TODO check why it is not working on Nomad dev but ok on uat
-        self.client.get(
-            '/en/search?query="hilton%20worldwide"&page=1&sort=desc&lang=en',
-            verify=False,
-        )
+    #     # TODO check why it is not working on Nomad dev but ok on uat
+    #     self.client.get(
+    #         '/en/search?query="hilton%20worldwide"&page=1&sort=desc&lang=en',
+    #         verify=False,
+    #     )
 
-        self.client.get(
-            "/en/document/treaty/en-qatar-turkey-bit-2001-qatar-turkey-bit-2001-tuesday-25th-december-2001",
-            verify=False,
-        )
+    #     self.client.get(
+    #         "/en/document/treaty/en-qatar-turkey-bit-2001-qatar-turkey-bit-2001-tuesday-25th-december-2001",
+    #         verify=False,
+    #     )
 
-        self.client.get(
-            "/en/api/documents/publications/juris?page=1&items=12&show_volumes=false&group_journals=false",
-            verify=False,
-        )
+    #     self.client.get(
+    #         "/en/api/documents/publications/juris?page=1&items=12&show_volumes=false&group_journals=false",
+    #         verify=False,
+    #     )
 
-        self.client.get(
-            "/en/document/publication/en-arbitrator-disclosure", verify=False
-        )
+    #     self.client.get(
+    #         "/en/document/publication/en-arbitrator-disclosure", verify=False
+    #     )
 
-        # Documents
-        # Big
+    #     # Documents
+    #     # Big
 
-        self.client.get(
-            "/en/document/decision/fr-victor-pey-casado-and-president-allende-foundation-v-republic-of-chile-award-thursday-8th-may-2008#decision_849",
-            verify=False,
-        )
+    #     self.client.get(
+    #         "/en/document/decision/fr-victor-pey-casado-and-president-allende-foundation-v-republic-of-chile-award-thursday-8th-may-2008#decision_849",
+    #         verify=False,
+    #     )
 
-        # Mid
-        self.client.get(
-            "/en/document/decision/fr-association-oxfam-france-association-notre-affaire-a-tous-fondation-pour-la-nature-et-lhomme-association-greenpeace-france-v-france-jugement-du-tribunal-administratif-de-paris-wednesday-3rd-february-2021",
-            verify=False,
-        )
+    #     # Mid
+    #     self.client.get(
+    #         "/en/document/decision/fr-association-oxfam-france-association-notre-affaire-a-tous-fondation-pour-la-nature-et-lhomme-association-greenpeace-france-v-france-jugement-du-tribunal-administratif-de-paris-wednesday-3rd-february-2021",
+    #         verify=False,
+    #     )
 
-        # Small
-        self.client.get(
-            "/en/document/treaty/en-libya-slovakia-bit-2009-libya-slovakia-bit-2009-friday-20th-february-2009",
-            verify=False,
-        )
+    #     # Small
+    #     self.client.get(
+    #         "/en/document/treaty/en-libya-slovakia-bit-2009-libya-slovakia-bit-2009-friday-20th-february-2009",
+    #         verify=False,
+    #     )
 
-        # self.client.get("/en/coverage/investment-arbitration", verify=False)
+    #     # self.client.get("/en/coverage/investment-arbitration", verify=False)
 
-        self.client.get("/en/partnership/icc", verify=False)
+    #     self.client.get("/en/partnership/icc", verify=False)
 
-        self.client.get("/en/jus-ai-assistant", verify=False)
+    #     self.client.get("/en/jus-ai-assistant", verify=False)
 
-        # self.client.post("/en/test?query=locus", verify=False)
+    #     # self.client.post("/en/test?query=locus", verify=False)
 
-        self.client.get("/en/directory/arbitrators/all", verify=False)
+    #     self.client.get("/en/directory/arbitrators/all", verify=False)
 
-        # self.client.get("/hello")
-        # self.client.get("/world")
+    #     # self.client.get("/hello")
+    #     # self.client.get("/world")
 
-    @task(3)
-    def jc_front(self):
+    # @task(3)
+    # def jc_front(self):
 
-        # JC Profiles
+    #     # JC Profiles
 
-        # Big
+    #     # Big
 
-        self.client.get(
-            "/en/d/profile/institution/en-icc-international-chamber-of-commerce",
-            verify=False,
-        )
+    #     self.client.get(
+    #         "/en/d/profile/institution/en-icc-international-chamber-of-commerce",
+    #         verify=False,
+    #     )
 
-        # Mid
+    #     # Mid
 
-        self.client.get("/en/p/alexis-mourre", verify=False)
+    #     self.client.get("/en/p/alexis-mourre", verify=False)
 
-        # Small
+    #     # Small
 
-        self.client.get("/en/p/daniel-jackson", verify=False)
+    #     self.client.get("/en/p/daniel-jackson", verify=False)
 
     # @task(4)
     # def jm_back(self):
