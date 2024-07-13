6766cdfa2939a349007ecea51ff5f3139e60b564
AlbanAndrieu <alban.andrieu@free.fr>
2024-07-13 22:20:31 +0200

    shellcheck

--- scripts/run-request.sh
+++ scripts/run-request.sh
@@ -2,6 +2,7 @@
 #set -xv
 
 TIMES=1
+# shellcheck disable=SC2250,SC2034
 for i in $(eval echo "{1..$TIMES}"); do
 	siege -c 1 -r 10 http://localhost:8080/
 	siege -c 3 -r 5 http://localhost:8080/v1/items/0
