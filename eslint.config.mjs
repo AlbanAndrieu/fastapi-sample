2451adb66f882cb1e1d1a81acd14fb1280e96d50
Alban Andrieu <alban.andrieu@free.fr>
2025-12-11 15:06:31 +0100

    Add new API endpoint to retrieve basic info from GitHub/GitLab repos

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,17 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+    {
+      ignores: [
+        '.direnv/*',
+        '.venv/*',
+        '.vscode/*',
+        'api/_lib/*',
+        '**/dist/*',
+        '**/tests/*',
+        'tsconfig.json',
+      ]
+    },
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
 );
