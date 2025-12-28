2233092acec34947023ff9ef7c25ef95ffac1bda
AlbanAndrieu <alban.andrieu@free.fr>
2025-12-28 12:35:44 +0100

    fix: Rename .python-version-direnv for vercel

--- my-app/src/entry.py
+++ my-app/src/entry.py
@@ -10,5 +10,5 @@ async def on_fetch(request, env):
         return Response("Hello, World!")
     # TODO: add a route to get the redis connection
     if url.path == "/v1/random":
-        return Response(uuid4())
+        return Response(uuid4().str())
     return Response("Not Found", status=404)
