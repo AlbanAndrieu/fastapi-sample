2137b192bbb954fed61789239b50d6c724f966a5
AlbanAndrieu <alban.andrieu@free.fr>
2026-01-12 20:49:31 +0100

    refactor(commitizen): Improve commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,52 +2,47 @@
 
 ### Fix
 
-- Update requirements.txt
 - uv
-- vercel
-- Rename .python-version-direnv for vercel
-- Helm lint
-- Helm lint
 
 ## [1.2.4](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.2.3...1.2.4) (2025-10-13)
 
-### Bug Fixes
+### Fix
 
 - Add vscode files ([43f71e4](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/43f71e4c7e9aaf1e80a5dd7e80d9abb60ea9d687))
 
 ## [1.2.3](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.2.2...1.2.3) (2025-10-13)
 
-### Bug Fixes
+### Fix
 
 - pytest ([b4b26b7](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/b4b26b7f8a16cdf4ab49dee44bd435909e1fc1b6))
 
 ## [1.2.2](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.2.1...1.2.2) (2025-10-13)
 
-### Bug Fixes
+### Fix
 
 - pipfile lock ([63a1c9b](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/63a1c9b14c95bd8cfb764c9a63d2460b3e082287))
 
 ## [1.2.1](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.2.0...1.2.1) (2025-10-10)
 
-### Bug Fixes
+### Fix
 
 - upgrade to version 1.2.0 ([f58d847](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/f58d84728d3c75db839ac1add2aa6d63f074ce5d))
 
 # [1.2.0](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.1.4...1.2.0) (2025-10-09)
 
-### Features
+### Feat
 
 - Add feature flags ([6ef3c40](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/6ef3c40ab4bb64cdc3c5ca11d1e5b04f52cd6551))
 
 # [1.2.0](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.1.4...1.2.0) (2025-10-08)
 
-### Features
+### Feat
 
 - Add feature flags ([6ef3c40](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/6ef3c40ab4bb64cdc3c5ca11d1e5b04f52cd6551))
 
 ## [1.1.4](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.1.3...1.1.4) (2025-10-07)
 
-### Bug Fixes
+### Fix
 
 - gitlab pages ([3215ffd](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/3215ffd3a65c1be4141d421184dead04ca4ec138))
 
@@ -55,13 +50,13 @@
 
 Docker images 6.02GB
 
-### Features
+### Feat
 
 - Add labchain with GPU and CUDA
 
 # [1.1.0](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/compare/1.0.5...1.1.0) (2024-12-10)
 
-### Features
+### Feat
 
 - **env:** add Go installation and configuration to .envrc for better development setup ([f321726](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/f3217266728970e901cf3c0245868f252a42b551))
 - **go.mod:** initialize Go module with module path and Go version ([7444150](https://gitlab.com/jusmundi-group/proof-of-concept/fastapi-sample/commit/7444150ce6b704827c9751bb2ad73991b7857484))
--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,5 @@
-import config from "commitlint-config-gitmoji";
+// import config from "commitlint-config-gitmoji";
+import config from "@commitlint/config-conventional";
 import type { ParserPreset, UserConfig } from "@commitlint/types";
 import createPreset from "conventional-changelog-conventionalcommits";
 import { merge } from "lodash-es";
@@ -32,7 +33,8 @@ async function createEmojiParser(): Promise<ParserPreset> {
 const emojiParser = await createEmojiParser();
 
 export default {
-	extends: ["./node_modules/commitlint-config-gitmoji"],
+	extends: ["@commitlint/config-conventional"],
+	//extends: ["./node_modules/commitlint-config-gitmoji", "@commitlint/config-conventional"],
 	parserPreset: {
 		emojiParser,
 		parserOpts: {
@@ -87,4 +89,107 @@ export default {
 		],
 		// "references-empty": [2, "never"],
 	},
+    prompt: {
+      settings: {},
+      messages: {
+        skip: ':skip',
+        max: 'upper %d chars',
+        min: '%d chars at least',
+        emptyWarning: 'can not be empty',
+        upperLimitWarning: 'over limit',
+        lowerLimitWarning: 'below limit'
+      },
+      questions: {
+        type: {
+          description: "Select the type of change that you're committing:",
+          enum: {
+            feat: {
+              description: 'A new feature',
+              title: 'Features',
+              emoji: '✨',
+            },
+            fix: {
+              description: 'A bug fix',
+              title: 'Bug Fixes',
+              emoji: '🐛',
+            },
+            docs: {
+              description: 'Documentation only changes',
+              title: 'Documentation',
+              emoji: '📚',
+            },
+            style: {
+              description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+              title: 'Styles',
+              emoji: '💎',
+            },
+            refactor: {
+              description: 'A code change that neither fixes a bug nor adds a feature',
+              title: 'Code Refactoring',
+              emoji: '📦',
+            },
+            perf: {
+              description: 'A code change that improves performance',
+              title: 'Performance Improvements',
+              emoji: '🚀',
+            },
+            test: {
+              description: 'Adding missing tests or correcting existing tests',
+              title: 'Tests',
+              emoji: '🚨',
+            },
+            build: {
+              description: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+              title: 'Builds',
+              emoji: '🛠',
+            },
+            ci: {
+              description: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+              title: 'Continuous Integrations',
+              emoji: '⚙️',
+            },
+            chore: {
+              description: "Other changes that don't modify src or test files",
+              title: 'Chores',
+              emoji: '♻️',
+            },
+            revert: {
+              description: 'Reverts a previous commit',
+              title: 'Reverts',
+              emoji: '🗑',
+            },
+          },
+        },
+        scope: {
+          description:
+            'What is the scope of this change (e.g. component or file name)',
+        },
+        subject: {
+          description: 'Write a short, imperative tense description of the change',
+        },
+        body: {
+          description: 'Provide a longer description of the change',
+        },
+        isBreaking: {
+          description: 'Are there any breaking changes?',
+        },
+        breakingBody: {
+          description:
+            'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
+        },
+        breaking: {
+          description: 'Describe the breaking changes',
+        },
+        isIssueAffected: {
+          description: 'Does this change affect any open issues?',
+        },
+        issuesBody: {
+          description:
+            'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
+        },
+        issues: {
+          description: 'Add issue references (e.g. "fix #123", "re #123".)',
+        },
+      },
+    }
 } satisfies UserConfig;
