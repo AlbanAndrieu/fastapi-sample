14ce3fee155649a4dd6d238c65d583af0553c5b9
AlbanAndrieu <alban.andrieu@free.fr>
2026-01-12 16:07:18 +0100

    fix: test new ci

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -40,20 +40,33 @@ export default {
 			issuePrefixes: ["ANDR-", "TEST-", "DSC-", "ABC-", "CO-"],
 		},
 	},
-	rules: {
+	rules: {		
+		//  Wrap the body at 72 characters.
+		"body-max-line-length": [2, 'always', 72],
+		//  Body is added by leaving a blank line after the subject line.
 		"body-leading-blank": [1, "always"],
+		// Ensure a blank line precedes the footer.
 		"footer-leading-blank": [1, "always"],
+		// Subject/Description Rules:
+        // Short and Summarized: Try to fit the subject line inside 72 characters.
 		"header-max-length": [2, "always", 72],
+		// Enforce that if a scope is used, it is in lower-case.
 		"scope-case": [2, "always", "lower-case"],
+		// Capitalize the description: Start subject line with a capital letter.
+        // 'sentence-case' helps with generating changelogs.
 		"subject-case": [
 			2,
-			"never",
-			["sentence-case", "start-case", "pascal-case", "upper-case"],
+			"always",
+			"sentence-case",
 		],
 		"subject-empty": [2, "never"],
+		 // Avoid trailing period.
 		"subject-full-stop": [2, "never", "."],
 		"type-case": [2, "always", "lower-case"],
+		// Format: <type>([optional scope]): <description> - enforced by most rules below.
+        // Enforce that the type is not empty.
 		"type-empty": [2, "never"],
+		// Enforce specific commit types. Add/remove types based on the project.
 		"type-enum": [
 			2,
 			"always",
