35961a91b7142113e734abe64965f80bbcc6b8a2
AlbanAndrieu <alban.andrieu@free.fr>
2026-01-12 19:29:13 +0100

    🎨 (config): update gitlint and commitlint configs for proper header length and emoji parsing
    Refine commitlint and gitlint settings to enforce a 72-character header length limit,
    enable emoji-based header parsing, and adjust ignored rules. Add commitlint as a
    dependency and fix configuration options for consistency and improved commit message
    validations across the project.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,9 +3,38 @@ import type { ParserPreset, UserConfig } from "@commitlint/types";
 import createPreset from "conventional-changelog-conventionalcommits";
 import { merge } from "lodash-es";
 
+// A helper function to create the custom emoji parser preset.
+async function createEmojiParser(): Promise<ParserPreset> {
+  // Generates the regex from the emojis defined in the conventional config.
+  const emojiRegexPart = Object.values(config.prompt.questions.type.enum)
+    .map((value) => value.emoji.trim())
+    .join("|");
+
+  const parserOpts = {
+    // This regular expression validates commit headers with an emoji.
+    breakingHeaderPattern: new RegExp(
+      `^(?:${emojiRegexPart})\\s+(\\w*)(?:\\((.*)\\))?!:\\s+(.*)$`,
+    ),
+    headerPattern: new RegExp(
+      `^(?:${emojiRegexPart})\\s+(\\w*)(?:\\((.*)\\))?!?:\\s+(.*)$`,
+    ),
+  };
+
+  const emojiParser = merge({}, await createPreset(), {
+    conventionalChangelog: { parserOpts },
+    parserOpts,
+    recommendedBumpOpts: { parserOpts },
+  });
+
+  return emojiParser;
+}
+
+const emojiParser = await createEmojiParser();
+
 export default {
 	extends: ["./node_modules/commitlint-config-gitmoji"],
 	parserPreset: {
+		emojiParser,
 		parserOpts: {
 			// these are samples, add possible prefixes based on your project requirement
 			issuePrefixes: ["ANDR-", "TEST-", "DSC-", "NABLA-", "JM-"],
@@ -19,8 +48,8 @@ export default {
 		// Ensure a blank line precedes the footer.
 		"footer-leading-blank": [1, "always"],
 		// Subject/Description Rules:
-        // Short and Summarized: Try to fit the subject line inside 100 characters (with emoji).
-		"header-max-length": [2, "always", 110],
+        // Short and Summarized: Try to fit the subject line inside 72 characters (80 with emoji).
+		"header-max-length": [2, "always", 80],
 		// Enforce that if a scope is used, it is in lower-case.
 		"scope-case": [2, "always", "lower-case"],
 		// Capitalize the description: Start subject line with a capital letter.
@@ -36,7 +65,7 @@ export default {
 		"type-case": [2, "always", "lower-case"],
 		// Format: <type>([optional scope]): <description> - enforced by most rules below.
         // Enforce that the type is not empty.
-		// "type-empty": [2, "never"],
+		"type-empty": [2, "never"],
 		// Enforce specific commit types. Add/remove types based on the project.
 		"type-enum": [
 			2,
@@ -56,5 +85,6 @@ export default {
 				"test",
 			],
 		],
+		// "references-empty": [2, "never"],
 	},
 } satisfies UserConfig;
