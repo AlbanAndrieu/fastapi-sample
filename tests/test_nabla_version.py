315a06343fe04ba746f43522f0a9b0a0be8390d4
AlbanAndrieu <alban.andrieu@free.fr>
2026-08-21 22:02:17 +0200

    test(release): align package version assertions

--- tests/test_nabla_version.py
+++ tests/test_nabla_version.py
@@ -3,7 +3,7 @@ from nabla.version import API_VERSION, RELEASE_VERSION, RUNTIME_VERSION
 
 
 def test_version():
-    assert __version__ == "1.3.7"
+    assert __version__ == "1.4.0"
     assert RELEASE_VERSION == __version__
     assert API_VERSION == "v0"
-    assert RUNTIME_VERSION == "v0+1.3.7"
+    assert RUNTIME_VERSION == "v0+1.4.0"
