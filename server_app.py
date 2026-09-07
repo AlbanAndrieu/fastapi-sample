1a6506251a9a985f72d7fb9ed92035b0a6a659e0
AlbanAndrieu <alban.andrieu@free.fr>
2026-09-07 20:46:23 +0200

    refactor(pyroscope): use shared application lifespan

--- server_app.py
+++ server_app.py
@@ -1,7 +1,6 @@
 import argparse
 import logging
 
-import pyroscope
 import uvicorn.config
 from pydantic import ValidationError
 
@@ -9,7 +8,6 @@ from nabla.config_settings import (
     APP_NAME,
     EXPOSE_HOST,
     EXPOSE_PORT,
-    PYROSCOPE_ENDPOINT,
     get_settings,
 )
 from nabla.main import app
@@ -24,16 +22,6 @@ def uvicorn_run() -> None:
 
     exit_code = 0
     try:
-        pyroscope.configure(
-            application_name=APP_NAME,
-            server_address=PYROSCOPE_ENDPOINT,  # See https://grafana.com/docs/pyroscope/next/configure-client/language-sdks/python/
-            # server_port=EXPOSE_PORT,
-            # service_name="fastapi-sample",
-            # trace_id_key="otelTraceID",
-            # span_id_key="otelSpanID",
-            sample_rate=100,  # default is 100
-        )
-
         setup_logging()
 
         try:
