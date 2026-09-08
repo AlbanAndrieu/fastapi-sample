8e99f718bcea54b0ffe39e5b261771390dcd1946
AlbanAndrieu <alban.andrieu@free.fr>
2026-09-08 16:31:08 +0200

    fix(ci): remove redundant sickz import aliases

--- nabla/api/sickz_policy.py
+++ nabla/api/sickz_policy.py
@@ -26,9 +26,9 @@ from nabla.api.homelab_catalog import fetch_homelab_services
 from nabla.api.homelab_models import HomelabService
 from nabla.api.homelab_runtime import ObservedApp, TrueNASRuntimeSnapshot, fetch_truenas_runtime
 from nabla.api.sickz_cloudflare_edge import (
-    _ANONYMOUS_EDGE_HEADERS as _ANONYMOUS_EDGE_HEADERS,
+    _ANONYMOUS_EDGE_HEADERS,
     _probe_http_edge_evidence,
-    _response_contains_cloudflare_default_deny as _response_contains_cloudflare_default_deny,
+    _response_contains_cloudflare_default_deny,
 )
 
 _DIRECT_EXTERNAL_SUFFIX = ".int.albandrieu.com"
